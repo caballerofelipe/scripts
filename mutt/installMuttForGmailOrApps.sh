9dbfbda8f5f849594c09861778dcb80d45d3c875
Felipe Caballero Gil <11650766+caballerofelipe@users.noreply.github.com>
2018-10-10 21:50:52 -0300

    Changed order and added dir creation.

--- mutt/installMuttForGmailOrApps.sh
+++ mutt/installMuttForGmailOrApps.sh
@@ -5,7 +5,7 @@ muttdirdefault='~/.mutt'
 # /Config
 
 echo "This script installs mutt and configures it to be used with Gmail or Google Apps"
-
+echo
 read -p '... Install mutt using Homebrew? [y/N]' muttInstall
 if [ "$muttInstall" != "y" ]; then
 	echo 'Exiting'
@@ -15,13 +15,24 @@ fi
 echo
 echo "... Please fill some details"
 read -p "Full username (username@domain):" userdomain
-echo "Note: you might need an App Password, see this: https://support.google.com/accounts/answer/185833"
+echo "(Note: you might need an App Password, see this: https://support.google.com/accounts/answer/185833)"
 read -s -p "Password (will be stored as text in file):" pass
 echo
 read -p "Full name:" fullname
 read -p "mutt dir (empty for default ~/.mutt)" muttdir
 if [ -z $muttdir ]; then
 	muttdir=$muttdirdefault
+fi
+
+muttrcFileExpanded=$(eval echo $muttrcFile)
+muttdirExpanded=$(eval echo $muttdir)
+if [ -s $muttrcFileExpanded ]; then
+	echo "$muttrcFile is not empty: \033[31maborting\033[0m"
+	exit
+fi
+if [ -e $muttdirExpanded ]; then
+	echo "mutt dir exists ($muttdirExpanded): \033[31maborting\033[0m"
+	exit
 fi
 
 echo
@@ -35,14 +46,8 @@ else
 	echo 'mutt install: \033[32mdone\033[0m'
 fi
 
-muttrcFileExpanded=$(eval echo $muttrcFile)
 echo
 echo "... Setting $muttrcFile"
-if [ -s $muttrcFileExpanded ]; then
-	echo "$muttrcFile is not empty: \033[31maborting\033[0m"
-	exit
-fi
-
 { # See https://stackoverflow.com/a/51113160/1071459
 	echo "set imap_user = \"$userdomain\""
 	echo "set imap_pass = \"$pass\""
@@ -53,9 +58,15 @@ fi
 	echo "set folder = \"imaps://imap.gmail.com:993\""
 	echo "set spoolfile = \"+INBOX\""
 	echo "set postponed = \"+[Gmail]/Drafts\""
-	echo "set header_cache = $muttdir/cache/headers"
-	echo "set message_cachedir = $muttdir/cache/bodies"
-	echo "set certificate_file = $muttdir/certificates"
+	echo "set header_cache = $muttdirExpanded/cache/headers"
+	echo "set message_cachedir = $muttdirExpanded/cache/bodies"
+	echo "set certificate_file = $muttdirExpanded/certificates"
 	echo "set move = no"
 	echo "set smtp_authenticators = 'gssapi:login'"
 } > $muttrcFileExpanded
+
+echo
+echo "... Creating directories in $muttdir"
+mkdir -p $muttdirExpanded/cache/headers
+mkdir -p $muttdirExpanded/cache/bodies
+mkdir -p $muttdirExpanded/certificates
