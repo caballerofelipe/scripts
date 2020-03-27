5650b06a8e28f6dccead1a1d11cb611fcf3f1adc
Felipe Caballero Gil <11650766+caballerofelipe@users.noreply.github.com>
2020-03-27 17:18:13 -0300

    Added two scripts to set timers (seconds and minutes).

---
+++ time/timer_minutes.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# ************************************ #
+# Timer in minutes
+# Usage:
+#	timer_minutes.sh [time in minutes]
+# Example, Possible usage to call a script after 10 minutes:
+#	timer_minutes.sh 10; echo "I'm done"
+# ************************************ #
+
+function timer_seconds {
+	if [ $1 = 0 ]; then
+		printf "%d:%02d\n" 0 0
+		return 0
+	elif [ $1 -lt 0 ]; then
+		echo 'Error: negative time'
+		return 1
+	fi
+	first_time=1
+	function showTimeLeft {
+		# Add a new line to keep cursor below, only the first time ran
+		if [[ $first_time = 1 ]]; then
+			printf "\n"
+			first_time=0
+		fi
+		time_sec_now=$(date +%s)
+		left_minutes=$(( ($time_sec_init + $seconds_to_break - $time_sec_now) / 60 ))
+		left_seconds=$(( ($time_sec_init + $seconds_to_break - $time_sec_now) % 60 ))
+		tput el # Delete to end of line
+		tput cuu1 # Up one line
+		printf "%d:%02d\n" $left_minutes $left_seconds
+	}
+
+	time_sec_init=$(date +%s)
+	seconds_to_break=$1
+	# Next line to emulate do-while
+	showTimeLeft
+	while sleep 1; do
+		showTimeLeft
+		# When timer done, To change timer 0:00 for another message use next line, comment the other one
+		# [[ $(($time_sec_init + $seconds_to_break - $time_sec_now)) = 0 ]] && tput cuu1 && tput el && echo "Time elapsed: $1 seconds!" && break
+		[[ $(($time_sec_init + $seconds_to_break - $time_sec_now)) -le 0 ]] && break
+	done
+}
+
+timer_seconds $((60 * $1))
